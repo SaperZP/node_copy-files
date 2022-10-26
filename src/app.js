906c95b062a50827810e4eb46608292df0f9e87b
Andrew Bilous <bilous.a.work@gmail.com>
2022-10-26 22:37:19 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const fs = require('fs');
+
+function callback(err) {
+  if (err) {
+    throw err;
+  }
+  // eslint-disable-next-line no-console
+  console.log('source.txt was copied to destination.txt');
+}
+
+fs.copyFile(
+  process.argv[2],
+  process.argv[3],
+  fs.constants.COPYFILE_EXCL,
+  callback,
+);
